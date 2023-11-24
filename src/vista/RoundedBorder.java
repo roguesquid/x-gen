6334bab62b20b6f8a22af6391d34f13ef3e4068a
Luis Martin <luisricardomartin@gmail.com>
2023-11-23 20:30:31 -0400

    Initial commit
    Adding all files to remote repository

---
+++ src/vista/RoundedBorder.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author luisr
+ */
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
+class RoundedBorder implements Border {
+    private int radius;
+    RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius, this.radius, this.radius, this.radius);
+    }
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
+}
